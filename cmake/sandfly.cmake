5528f41968eac084f25cb03ce336bb7f0547072f
Noah Oblath <nsoblath@gmail.com>
2020-09-08 17:20:09 -0700

    Use SANDFLY_DIR to include package builder

--- cmake/sandfly.cmake
+++ cmake/sandfly.cmake
@@ -4,7 +4,10 @@
 
 # Main directory for Sandfly
 set( SANDFLY_DIR ${CMAKE_CURRENT_LIST_DIR}/.. )
-MESSAGE( STATUS "sandfly dir: ${SANDFLY_DIR}" )
+message( STATUS "sandfly dir: ${SANDFLY_DIR}" )
+
+list( APPEND CMAKE_MODULE_PATH ${SANDFLY_DIR}/midge/scarab/cmake )
+include( PackageBuilder )
 
 macro (sandfly_build_executables)
 	add_subdirectory( ${SANDFLY_DIR}/executables )
