7be65e85e52ffee826f07519cc176d734774662f
Noah Oblath <nsoblath@gmail.com>
2020-09-10 11:55:03 -0700

    New function for creating the sandfly executable

--- cmake/sandfly.cmake
+++ cmake/sandfly.cmake
@@ -3,12 +3,61 @@
 # Author: N.S. Oblath
 
 # Main directory for Sandfly
-set( SANDFLY_DIR ${CMAKE_CURRENT_LIST_DIR}/.. )
-message( STATUS "sandfly dir: ${SANDFLY_DIR}" )
+set( SANDFLY_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/.. )
+message( STATUS "sandfly dir: ${SANDFLY_SOURCE_DIR}" )
 
-list( APPEND CMAKE_MODULE_PATH ${SANDFLY_DIR}/midge/scarab/cmake )
+list( APPEND CMAKE_MODULE_PATH ${SANDFLY_SOURCE_DIR}/midge/scarab/cmake )
 include( PackageBuilder )
 
-macro (sandfly_build_executables)
-	add_subdirectory( ${SANDFLY_DIR}/executables )
-endmacro ()
+function( build_sandfly_executable )
+	# Builds a the sandfly executable
+	#
+	# User is responsible for including the resulting executable in a target export.
+    #
+    # Parameters
+	#     SANDFLY_SUBMODULE_NAME: libraries to be linked against
+	#     ALT_NAME: 
+	#     ALT_SOURCES: 
+	#     PROJECT_LIBRARIES: 
+	#     EXTERNAL_LIBRARIES: 
+	#
+
+	set( OPTIONS )
+	set( ONEVALUEARGS SANDFLY_SUBMODULE_NAME ALT_NAME )
+	set( MULTIVALUEARGS ALT_SOURCES PROJECT_LIBRARIES EXTERNAL_LIBRARIES )
+	cmake_parse_arguments( EXE "${OPTIONS}" "${ONEVALUEARGS}" "${MULTIVALUEARGS}" ${ARGN} )
+
+	if( NOT EXE_ALT_NAME )
+		set( EXE_ALT_NAME "sandfly" )
+	endif()
+
+	message( STATUS "Building main sandfly executable as <${ALT_EXE_NAME}> from project <${CMAKE_PROJECT_NAME}>" )
+
+	if( NOT EXE_ALT_SOURCES )
+		set( EXE_ALT_SOURCES
+			sandfly.cc
+		)
+	endif()
+
+	if( CMAKE_PROJECT_NAME STREQUAL "Sandfly" )
+		list( APPEND EXE_PROJECT_LIBRARIES SandflyControl SandflyUtility )
+		message( STATUS "Building Sandfly executable(s) as part of Sandfly" )
+	else()
+		if( NOT EXE_SANDFLY_SUBMODULE_NAME )
+			message( FATAL_ERROR "Sandfly project name was not specified when building Sandfly executables.  Check the call to sandfly_build_executables()" )
+		endif()
+		pbuilder_use_sm_library( SandflyControl ${EXE_SANDFLY_SUBMODULE_NAME} )
+		pbuilder_use_sm_library( SandflyUtility ${EXE_SANDFLY_SUBMODULE_NAME} )
+		message( STATUS "Building Sandfly executable(s) as part of ${CMAKE_PROJECT_NAME}" )
+	endif()
+
+	set( sandfly_exe_PROGRAMS )
+	pbuilder_executable( 
+		SOURCES ${EXE_ALT_SOURCES}
+		EXECUTABLE ${EXE_ALT_NAME}
+		PROJECT_LIBRARIES ${EXE_PROJECT_LIBRARIES}
+		PUBLIC_EXTERNAL_LIBRARIES ${EXE_EXTERNAL_LIBRARIES}
+		#PRIVATE_EXTERNAL_LIBRARIES ${PRIVATE_EXT_LIBS}
+	)
+
+endfunction()
